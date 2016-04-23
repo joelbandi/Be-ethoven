2ad1584c0e417cdb2145ec9fcac58b68c23e6949
joelbandi <joelvivekbandi@gmail.com>
2016-04-23 05:52:14 -0700

    Added the 6 sensor spaces

--- src/LeapListener.java
+++ src/LeapListener.java
@@ -7,6 +7,8 @@ public class LeapListener extends Listener {
 		System.out.println("Controller connected");
 	}
 	
+	
+	
 	public  void onFrame(Controller controller) {
  
 		Frame frame = controller.frame();
@@ -18,61 +20,67 @@ public class LeapListener extends Listener {
 			System.out.println("Put both your hands in range");
 			return;
 		}
-		
-		
-		
-		
 		Hand righthand  = handlist.rightmost();
 		Hand lefthand = handlist.leftmost();
-		
-
-		
-		
-		
-		
-		
-		
+		Hand hand = righthand;
 		Vector rightvector = righthand.palmPosition();
 		Vector leftvector = lefthand.palmPosition();
-		
-		
 		float rightx = rightvector.getX();
 		float leftx =  leftvector.getX();
 		
-		
-		
+		float righty = rightvector.getY();
+		float lefty = leftvector.getY();
 		
 		System.out.println("Scanning this hand");
-		
 		boolean chk = righthand.isValid() && righthand.isValid();
-		
 		String r = "";
 		String l = "";
 		
-		if(rightx>150.0 && chk){
-			 r = "->";
-		}else if(rightx<100.0 && chk){
-			 r = "<-";
-		}else if(chk){
-			r = "--";
+		if(righty<=150.0){
+			if(rightx>150.0 && chk){
+				 r = "6";
+			}else if(rightx<100.0 && chk){
+				 r = "4";
+			}else if(chk){
+				r = "5";
+			}
+		}else{
+			if(rightx>150.0 && chk){
+				 r = "6#";
+			}else if(rightx<100.0 && chk){
+				 r = "4#";
+			}else if(chk){
+				r = "5#";
+			}
 		}
 		
-		if(leftx<-150.0 && chk){
-			l = "<-";
-		}else if(leftx>-100.0 && chk){
-			l = "->";
-		}else if(chk){
-			l = "--";
+		
+		if(lefty<=150.0){
+			if(leftx<-150.0 && chk){
+				l = "1";
+			}else if(leftx>-100.0 && chk){
+				l = "3";
+			}else if(chk){
+				l = "2";
+			}
+		}else{
+			if(leftx<-150.0 && chk){
+				l = "1#";
+			}else if(leftx>-100.0 && chk){
+				l = "3#";
+			}else if(chk){
+				l = "2#";
+			}
 		}
 		
 		
+	
 		
+
 		System.out.println(l+" and "+r);
-		
-		
-		
-        
-        
     }
+	
+	
+	
 	
 }
